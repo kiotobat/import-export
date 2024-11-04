a86570ab8d4bcc6d75aaad9c3cb3deec5dde69bc
Spartak I. <lambar926@gmail.com>
2024-11-04 21:00:08 +0300

    first commit

---
+++ game.js
@@ -0,0 +1,13 @@
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+}
+  
+export class GameSavingData {}
+  
+export function readGameSaving() {}
+  
+export function writeGameSaving() {}
+
+import Character from "./domain"; 
